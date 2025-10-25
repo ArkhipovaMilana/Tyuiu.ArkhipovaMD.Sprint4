ecf01235192abcba2bbcab45e78ce6e150dbaa22
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-25 23:55:21 +0500

    Add project files.

---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task0.V11.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ArkhipovaMD.Sprint4.Task0.V11.Lib
+{
+    public class DataService : ISprint4Task0V11
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int resMul = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    resMul *= array[i];
+                }
+            }
+            return resMul;
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task0.V11/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint4.Task0.V11.Lib;
+//{ 8, 9, 5, 4, 3, 2, 1, 0, 5, 7}
+DataService ds = new DataService();
+int[] array = new int[10];
+array[0] = 8;
+array[1] = 9;
+array[2] = 5;
+array[3] = 4;
+array[4] = 3;
+array[5] = 2;
+array[6] = 1;
+array[7] = 0;
+array[8] = 5;
+array[9] = 7;
+Console.WriteLine(ds.GetMultOddArrEl(array));
