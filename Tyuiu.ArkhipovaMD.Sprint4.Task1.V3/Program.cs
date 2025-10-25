c8869e3990c7d894ac26ced82362c652420814ef
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-26 01:25:15 +0500

    Task1

---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint4.Task1.V3.Lib;
+
+DataService ds  = new DataService();
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+for (int i = 0; i < len; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(ds.Calculate(array));
