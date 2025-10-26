403858ad721b704caa7f2260e2483112e21dda4c
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-26 20:44:44 +0500

    Task4

---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ArkhipovaMD.Sprint4.Task4.V27.Lib
+{
+    public class DataService : ISprint4Task4V27
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sumRes = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                { if (matrix[i,j]%2!=0)
+                    {
+                        sumRes += matrix[i,j];
+                    }
+                }
+            }
+            return sumRes;
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task4.V27/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint4.Task4.V27.Lib;
+
+//DataService ds  = new DataService();
+//int[,] array = new int[5,5];
+//for (int i = 0; i < 5; i++)
+//{
+//    for (int j = 0; j < 5; j++)
+//    {
+//        array[i, j] = Convert.ToInt32(Console.ReadLine());
+//    }
+//}
+//Console.WriteLine(ds.Calculate(array));
+
+int resm = 0;
+for (int i = 0;i < 25; i++)
+{
+
+     int u = Convert.ToInt32(Console.ReadLine());
+     if (u%2!= 0)
+     {
+        resm += u;
+     }
+}
+Console.WriteLine(resm);
