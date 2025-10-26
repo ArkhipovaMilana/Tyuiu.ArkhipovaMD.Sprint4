321184ffb31f09d23d2446d49e4f200ca213d932
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-26 20:19:27 +0500

    Task3

---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task3.V21.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ArkhipovaMD.Sprint4.Task3.V21.Lib
+{
+    public class DataService : ISprint4Task3V21
+    {
+        public int Calculate(int[,] array)
+        {
+            int res = 1;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                res *= array[i, array.GetLength(1)-1];
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task3.V21/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint4.Task3.V21.Lib;
+
+DataService ds  = new DataService();
+int[,] ar = new int[5, 5]
+{
+{ 8, 3, 3, 3, 5 },
+{ 6, 7, 3, 3, 7 },
+{ 4, 5, 4, 6, 6 },
+{ 5, 5, 8, 8, 7 },
+{ 4, 6, 6, 7, 7 }
+};
+Console.WriteLine(ds.Calculate(ar));
