a5a8352bb14e7154114bc9f45bfd8bd970966e1e
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-26 20:46:05 +0500

    Task4

--- Tyuiu.ArkhipovaMD.Sprint4.Task4.V27/Program.cs
+++ Tyuiu.ArkhipovaMD.Sprint4.Task4.V27/Program.cs
@@ -1,23 +1,13 @@
 ﻿using Tyuiu.ArkhipovaMD.Sprint4.Task4.V27.Lib;
 
-//DataService ds  = new DataService();
-//int[,] array = new int[5,5];
-//for (int i = 0; i < 5; i++)
-//{
-//    for (int j = 0; j < 5; j++)
-//    {
-//        array[i, j] = Convert.ToInt32(Console.ReadLine());
-//    }
-//}
-//Console.WriteLine(ds.Calculate(array));
-
-int resm = 0;
-for (int i = 0;i < 25; i++)
+DataService ds = new DataService();
+int[,] array = new int[5, 5];
+for (int i = 0; i < 5; i++)
 {
-     int u = Convert.ToInt32(Console.ReadLine());
-     if (u%2!= 0)
-     {
-        resm += u;
-     }
+    for (int j = 0; j < 5; j++)
+    {
+        array[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
 }
-Console.WriteLine(resm);
+Console.WriteLine(ds.Calculate(array));
+
