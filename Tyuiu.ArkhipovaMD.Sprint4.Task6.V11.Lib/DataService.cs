e01bd4904aac8fc16fc0d81e03db48c2a0773171
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-27 19:25:57 +0500

    Task6

---
+++ Tyuiu.ArkhipovaMD.Sprint4.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ArkhipovaMD.Sprint4.Task6.V11.Lib
+{
+    public class DataService : ISprint4Task6V11
+    {
+        public int Calculate(string[] array)
+        {
+            int cnt = 0;
+            foreach (var item in array)
+            {
+                if (item.Length == 5)
+                {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+}
